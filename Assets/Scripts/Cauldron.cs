529795ea05043f9e41a910ee3b4b93340ee0c139
Evelyne Berger-Uichanco <evelyne.rose@ymail.com>
2016-04-25 13:26:31 +1000

    more comments. capped property intensity at 100%

--- Assets/Scripts/Cauldron.cs
+++ Assets/Scripts/Cauldron.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 using System;
 
 public class Cauldron : MonoBehaviour {
+    public const int unlikely = 25;
+    public const int weak = 50;
+    public const int medium = 75;
+    public const int strong = 100;
     SortedDictionary<Book.symptoms, int> properties;
     
 	// Use this for initialization
@@ -16,10 +20,12 @@ public class Cauldron : MonoBehaviour {
 	
 	}
 
+    //remove everything from the cauldron
     public void clearProperties() {
         properties.Clear();
     }
 
+    //add a property, or update, as needed
     public void addProperty(Book.symptoms symptom, int value) {
         if (properties.ContainsKey(symptom)) {
             updateProperty(symptom, value);
@@ -28,13 +34,17 @@ public class Cauldron : MonoBehaviour {
         }
     }
 
+    //private helper to update an existing property, or remove it if its effect is cancelled
+    //property intensity capped at 100%
     private void updateProperty(Book.symptoms symptom, int value) {
         properties[symptom] += value;
+        properties[symptom] = Math.Min(properties[symptom], strong);
         if(properties[symptom] == 0) {
             properties.Remove(symptom);
         }
     }
 
+    //returns an array of all the symptoms in the cauldron
     public Book.symptoms[] GetSymptoms() {
         Book.symptoms[] s = new Book.symptoms[properties.Count];
         int i = 0;
@@ -42,8 +52,10 @@ public class Cauldron : MonoBehaviour {
             s[i] = symptom;
             i++;
         }
-        return s;
+        return s;//UNUSED
     }
+
+    //returns an array of all the values associated with the symptoms in the cauldron
     public Book.symptoms[] GetValues() {
         Book.symptoms[] v = new Book.symptoms[properties.Count];
         int i = 0;
@@ -51,16 +63,33 @@ public class Cauldron : MonoBehaviour {
             v[i] = symptom;
             i++;
         }
-        return v;
+        return v;//UNUSED
     }
 
+    //returns the intesity of an int as a string
+    private string getIntensity(int intensity) {
+        int i = Mathf.Abs(intensity);
+        if (i <= unlikely) {
+            return "Unlikely";
+        }else if(i <= weak) {
+            return "Weak";
+        }else if(i <= medium) {
+            return "Medium";
+        } else {
+            return "Strong";
+        }
+    }
+
+    //override tostring method
     public override string ToString() {
         string str = "Cauldron Properties: \n";
         foreach(Book.symptoms symptom in properties.Keys) {
             if(properties[symptom] < 0) {
-                str += Mathf.Abs(properties[symptom]).ToString() + "% chance of curing ";
+                //str += Mathf.Abs(properties[symptom]).ToString() + "% chance of curing ";
+                str += getIntensity(properties[symptom]) + " chance of curing ";
             } else {
-                str += Mathf.Abs(properties[symptom]).ToString() + "% chance of causing ";
+                //str += Mathf.Abs(properties[symptom]).ToString() + "% chance of causing ";
+                str += getIntensity(properties[symptom]) + " chance of causing ";
             }
             str += symptom.ToString()+ "\n";
         }
